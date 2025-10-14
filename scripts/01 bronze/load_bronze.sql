bb52f90a10d434d922335fa0d15aef4540a6131d
Andrii Lesniak <todmount@gmail.com>
2025-10-14 22:18:15 +0300

    feat: Make load_bronze a stored procedure

--- scripts/01 bronze/load_bronze.sql
+++ scripts/01 bronze/load_bronze.sql
@@ -10,58 +10,94 @@ Script Purpose:
 ===============================================================================
 */
 
-USE DataWarehouse
 
-TRUNCATE TABLE  bronze.crm_cust_info;
-BULK INSERT bronze.crm_cust_info
-FROM '/datasets/source_crm/cust_info.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-    );
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+    DECLARE @SEP_BOLD VARCHAR(50), @SEP VARCHAR(50);
+    SET @SEP_BOLD = REPLICATE('=', 50);
+    SET @SEP = REPLICATE('-', 50);
 
-TRUNCATE TABLE  bronze.crm_prd_info;
-BULK INSERT bronze.crm_prd_info
-FROM '/datasets/source_crm/prd_info.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-    );
+    PRINT @SEP_BOLD;
+    PRINT 'Loading Bronze Level';
+    PRINT @SEP_BOLD;
 
-TRUNCATE TABLE  bronze.crm_sales_details;
-BULK INSERT bronze.crm_sales_details
-FROM '/datasets/source_crm/sales_details.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-    );
+    PRINT @SEP;
+    PRINT 'Loading CRM Tables';
+    PRINT @SEP;
 
-TRUNCATE TABLE  bronze.erp_CUST_AZ12;
-BULK INSERT bronze.erp_CUST_AZ12
-FROM '/datasets/source_erp/CUST_AZ12.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-    );
+    PRINT 'Truncating table: bronze.crm_cust_info'
+    TRUNCATE TABLE bronze.crm_cust_info;
 
-TRUNCATE TABLE  bronze.erp_LOC_A101;
-BULK INSERT bronze.erp_LOC_A101
-FROM '/datasets/source_erp/LOC_A101.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-    );
+    PRINT 'Inserting data into: bronze.crm_cust_info'
+    BULK INSERT bronze.crm_cust_info
+    FROM '/datasets/source_crm/cust_info.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+        );
 
-TRUNCATE TABLE  bronze.erp_PX_CAT_G1V2;
-BULK INSERT bronze.erp_PX_CAT_G1V2
-FROM '/datasets/source_erp/PX_CAT_G1V2.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-    );
+    PRINT 'Truncating table: bronze.crm_prd_info'
+    TRUNCATE TABLE bronze.crm_prd_info;
+
+    PRINT 'Inserting data into: bronze.crm_prd_info'
+    BULK INSERT bronze.crm_prd_info
+    FROM '/datasets/source_crm/prd_info.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+        );
+
+    PRINT 'Truncating table: bronze.crm_sales_details'
+    TRUNCATE TABLE bronze.crm_sales_details;
+
+    PRINT 'Inserting data into: bronze.crm_sales_details'
+    BULK INSERT bronze.crm_sales_details
+    FROM '/datasets/source_crm/sales_details.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+        );
+
+    PRINT @SEP;
+    PRINT 'Loading ERP Tables';
+    PRINT @SEP;
+
+    PRINT 'Truncating table: bronze.erp_CUST_AZ12'
+    TRUNCATE TABLE  bronze.erp_CUST_AZ12;
+
+    PRINT 'Inserting data into: bronze.erp_CUST_AZ12'
+    BULK INSERT bronze.erp_CUST_AZ12
+    FROM '/datasets/source_erp/CUST_AZ12.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+        );
+
+    PRINT 'Truncating table: bronze.erp_LOC_A101'
+    TRUNCATE TABLE bronze.erp_LOC_A101;
+
+    PRINT 'Inserting data into: bronze.erp_LOC_A101'
+    BULK INSERT bronze.erp_LOC_A101
+    FROM '/datasets/source_erp/LOC_A101.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+        );
+
+    PRINT 'Truncating table: bronze.erp_PX_CAT_G1V2'
+    TRUNCATE TABLE bronze.erp_PX_CAT_G1V2;
+
+    PRINT 'Inserting data into: bronze.erp_PX_CAT_G1V2'
+    BULK INSERT bronze.erp_PX_CAT_G1V2
+    FROM '/datasets/source_erp/PX_CAT_G1V2.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+        );
+END;
